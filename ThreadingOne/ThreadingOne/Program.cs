3e5536cf3c672d4d13b8a1346ae9006a950e092f
Jason Bell <jbell@accelebrate.com>
2016-04-07 23:01:12 +0000

    Cancel

---
+++ ThreadingOne/ThreadingOne/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Log("Started Main");
+            var cts = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(obj => ComputeBoundOp(cts.Token));
+            Console.WriteLine("Press any key to cancel");
+            Console.ReadKey();
+            cts.Cancel();
+            //Log("Started other work on main");
+            //Thread.Sleep(2000);
+            //Log("Finished other work on main");
+            Log("Finished Main");
+            Console.ReadKey();
+        }
+
+        static void Log(string msg)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] " + msg);
+        }
+
+        static void ComputeBoundOp(CancellationToken token)
+        {
+            Log("Started ComputeBoundOp");
+            for (int i = 0; i < 5; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Log("ComputeBoundOp cancelled");
+                    break;
+                }
+                Thread.Sleep(1000);
+                Log("ComputeBoundOp (" + i + ")");
+            }
+            Log("Finished ComputeBoundOp");
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void DoStuff()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    break;                 
+                }
+                backgroundWorker1.ReportProgress(i * 20);
+                Thread.Sleep(1000);
+            }           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DoStuff();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "Done";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = e.ProgressPercentage.ToString();
+        }
+    }
+}
